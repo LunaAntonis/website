18992a06b1ec914a614df0c6107c217773b3fa44
Luna Antonis <r0900845@student.thomasmore.be>
2023-09-19 15:40:19 +0200

    extra files

---
+++ les_01_html/oefening01_01/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1. Declaratie
+int getal1, getal2, getal3, getal4, som, product;
+
+// 2.Inlezen.
+/*Console.Write("Geef getal */1 in:");
+getal1 = int.Parse(Console.ReadLine());
+
+Console.Write("Geef getal 2 in:");
+getal2 = int.Parse(Console.ReadLine());
+
+//Console.Write("Geef getal 3 in:");
+getal3 = int.Parse(Console.ReadLine());
+
+//Console.Write("Geef getal 4 in:");
+getal4 = int.Parse(Console.ReadLine());
+
+//3.Berekeningen uitvoeren
+som = getal1 + getal3;
+product = getal2 * getal4;
+
+//4. Resultaat printen
+Console.WriteLine($"Optelling: {som}");
+Console.WriteLine($"Vermenigvuldiging: {product}");
+
+Console.WriteLine("Druk op Enter om af te sluiten.");
+Console.ReadLine();
